64324d1943a619d73e1cc2d2d894a43ce24fd0d5
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-18 19:50:26 +0200

    all IT done and some styling

--- routes/drones.js
+++ routes/drones.js
@@ -27,17 +27,35 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params 
+    Drones.findById(id)
+    .then((data)=>{
+        res.render('./drones/update-form.hbs', {data})
+    })
+    .catch((err)=> console.log(err))
+
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params 
+  const {name, propellers, maxSpeed} = req.body;
+    
+    Drones.findByIdAndUpdate(id, {name, propellers, maxSpeed})
+    .then((data)=>{
+        res.redirect('/drones')
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const {id} = req.params
+  Drones.findByIdAndRemove(id)
+  .then((data) => res.redirect('/drones'),)
+  .catch((err)=>console.log(err))
 });
 
 module.exports = router;
